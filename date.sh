844d8346c982abab6261c11210785ccc4543b2cf
Janeq95 <135978731+Janeq95@users.noreply.github.com>
2023-06-30 20:22:54 +0200

    Update date.sh
    update1

--- date.sh
+++ date.sh
@@ -2,3 +2,21 @@
 
 current_date=$(date +"%Y-%m-%d")
 echo "Dzisiejsza data: $current_date"
+
+while [[ $# -gt 0 ]]; do
+  key="$1"
+  case $key in
+    -d|--date)
+      echo "Dzisiejsza data: $current_date"
+      exit 0
+      ;;
+    *)
+      echo "Nieznana opcja: $key"
+      exit 1
+      ;;
+  esac
+  shift
+done
+
+# Jeśli nie podano żadnej opcji, domyślnie wyświetlana jest dzisiejsza data
+echo "Dzisiejsza data: $current_date"
